5aed97c8045a371f60bf3d6835df75da7fc99de1
Aloxbro <alox51@wanadoo.fr>
2021-05-27 23:01:10 +0200

    Added eslint + prettier + husky to lint every commit

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-import { ApolloServer } from "apollo-server";
-import mongoose from "mongoose";
-import { buildSchema } from "type-graphql";
-import "reflect-metadata";
-import { PostResolver } from "./PostResolver";
+import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
+import { buildSchema } from 'type-graphql';
+import 'reflect-metadata';
+import { PostResolver } from './PostResolver';
 
-async function start () {
-
-mongoose
+async function start() {
+  mongoose
     .connect('mongodb://127.0.0.1:27017/modussey', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    autoIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      autoIndex: true,
     })
-.then(() => console.log('Connected to database'))
-.catch((err: Error) => console.log(err));
+    .then(() => console.log('Connected to database'))
+    .catch((err: Error) => console.log(err));
 
-    const schema = await buildSchema({
-        resolvers: [PostResolver],
-      });
-      // Create the GraphQL server
-    const server = new ApolloServer({
-        schema,
-        playground: true,
-    });
+  const schema = await buildSchema({
+    resolvers: [PostResolver],
+  });
+  // Create the GraphQL server
+  const server = new ApolloServer({
+    schema,
+    playground: true,
+  });
 
-    // Start the server
-    const { url } = await server.listen(4000);
-    console.log(`Server is running, GraphQL Playground available at ${url}`);
+  // Start the server
+  const { url } = await server.listen(4000);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
 }
 
-start()
+start();
